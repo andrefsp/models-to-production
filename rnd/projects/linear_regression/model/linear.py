4219864dcc436fc0ed0a076f2dc3f4baf62c6a5f
andrefsp <andrefsp@gmail.com>
2019-09-30 11:00:11 +0100

    Linear regression add example with plotting

--- rnd/projects/linear_regression/model/linear.py
+++ rnd/projects/linear_regression/model/linear.py
@@ -1,9 +1,37 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 from common.model_builder import Model
 
 
+POINTS = [p for p in range(50)]
+
+
 class LinearModel(Model):
+
+    def plot(self, session, points=POINTS, pause=0.1, target_iterator=None):
+
+        x_train = []
+        y_train = []
+        for row in [r for r in target_iterator()][:len(POINTS)]:
+            x_train.append(float(row['X']))
+            y_train.append(float(row['Y']))
+
+        y = session.run(self.model, feed_dict={self.X: points})
+
+        plt.axis([0, len(POINTS), 0, len(POINTS)])
+
+        plt.scatter(x_train, y_train, c='g')
+        plt.plot(points, y)
+
+        plt.show(block=False)
+
+        plt.xticks(range(0, len(POINTS), 10))
+        plt.yticks(range(0, len(POINTS), 10))
+
+        plt.draw()
+        plt.pause(pause)
+        plt.clf()
 
     def build(self):
         # Input Placeholder (data points)
@@ -80,6 +108,7 @@ class LinearModel(Model):
         print("start:\t Y = %s*X + %s" % (
             session.run(self.M), session.run(self.B))
         )
+
         for epoch in range(self.config.epochs):
             if epoch % 100 == 0:
                 print("\t Epoch %s ::: \tY = %s*X + %s" % (
@@ -89,6 +118,9 @@ class LinearModel(Model):
                 # run evaluation at the each 100 epochs...
                 print("\t\t %s" % self.evaluate(session, dev_data_iterator))
 
+                # show current function
+                self.plot(session, target_iterator=train_data_iterator)
+
             x_train = []
             y_train = []
             for row in train_data_iterator():
@@ -97,8 +129,10 @@ class LinearModel(Model):
 
             session.run(
                 self.training,
-                feed_dict={self.X: x_train, self.Y: y_train}
-            )
+                feed_dict={
+                    self.X: x_train,
+                    self.Y: y_train
+                })
 
         self.save_tf_export(session)
 
